4a615b2049cf4cc7f3668cbf5ea3e1144d526942
João Denis Rodrigues <joao.denisr@gmail.com>
2017-08-17 12:28:16 -0300

    Merge historico e matricula + drop coluna nao utilizada

--- script/base/dataframe_base.py
+++ script/base/dataframe_base.py
@@ -33,6 +33,20 @@ def read_csv(path):
 def fix_dataframes(dataframes):
     for df in dataframes:
         fix_situation(df['dataframe'])
+        fix_admission(df['dataframe'])
+        fix_evasion(df['dataframe'])
+        if df['name'] == 'historico.xls':
+            hist = df['dataframe']
+        if df['name'] == 'matricula.xls':
+            mat = df['dataframe']
+    merged = pd.merge(hist, mat, on=['MATR_ALUNO'])
+    merged.drop(['ID_PESSOA', 'ID_CURRIC_ALUNO', 'CONCEITO', 'NOME_UNIDADE',
+                 'ID_NOTA', 'ID_VERSAO_CURSO', 'NOME_PESSOA', 'SIGLA',
+                 'NUM_VERSAO_y', 'COD_CURSO_y', 'DT_NASCIMENTO'
+                ], axis=1, inplace=True)
+    merged.rename(columns={'NUM_VERSAO_x':'NUM_VERSAO',
+                           'COD_CURSO_x':'COD_CURSO'}, inplace=True)
+    print(list(merged))
 
 def fix_situation(df):
     if hasattr(df, 'SITUACAO'):
@@ -41,3 +55,11 @@ def fix_situation(df):
             if situation[1] == 'Outro':
                 temp = df[~df['SITUACAO'].astype(str).str.isdigit()]
                 df.loc[~df.SITUACAO.astype(str).str.isdigit()] = situation[0]
+
+def fix_admission(df):
+    pass
+
+def fix_evasion(df):
+    pass
+
+
