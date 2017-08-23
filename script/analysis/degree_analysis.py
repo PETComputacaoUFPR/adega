ab12cc3d1f12d426b62cfc139fc5186c833bfec2
João Denis Rodrigues <joao.denisr@gmail.com>
2017-08-23 15:15:24 -0300

    Corrigido funcoes curso

--- script/analysis/degree_analysis.py
+++ script/analysis/degree_analysis.py
@@ -12,9 +12,9 @@ def average_graduation(df):
 
 
 def general_failure(df):
-    not_nan = df.dropna(axis=0)
-    affect_ira = not_nan[not_nan.SITUACAO.isin(Situation.SITUATION_AFFECT_IRA)]
+    affect_ira = df[df.SITUACAO.isin(Situation.SITUATION_AFFECT_IRA)]
     failures = affect_ira[affect_ira.SITUACAO.isin(Situation.SITUATION_FAIL)]
+
     average = failures.shape[0] / affect_ira.shape[0]
 
     student_courses = affect_ira.groupby(['MATR_ALUNO'], as_index=False)\
@@ -31,6 +31,6 @@ def general_failure(df):
 
 
 def general_ira(df):
-    fixed = df.dropna(axis=0)[df.SITUACAO.isin(Situation.SITUATION_AFFECT_IRA)]
+    fixed = df[df.SITUACAO.isin(Situation.SITUATION_AFFECT_IRA)]
     fixed = fixed[fixed.MEDIA_FINAL <= 100]
     return (fixed.MEDIA_FINAL.mean(), fixed.MEDIA_FINAL.std())
