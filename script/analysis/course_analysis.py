ff73b5137d51a06cbe96fc8c6a88e7f2715ecff0
odair mario <badwolves123@gmail.com>
2017-09-18 10:18:32 -0300

    analises iniciais

--- script/analysis/course_analysis.py
+++ script/analysis/course_analysis.py
@@ -1,16 +1,31 @@
 # -*- coding: utf-8 -*-
 
-import pandas as pd 
+import pandas as pd
 import numpy as np
+df = pd.read_excel("../base/historico.xls")
 
+# imprime completamente um dataframe
 def print_analise(d):
 	with pd.option_context('display.max_rows', None, 'display.max_columns', 27):
 		print(d)
+
+# calcula as taxas
+def func(x,matr):
+	c = matr[x['COD_ATIV_CURRIC']].values[0]
+	return (x['counts'] / c)
+
+#quantidade de matriculas
+def qnt_matr(df):
+	return df.groupby(['COD_ATIV_CURRIC']).size()
+
+
 def analise(df):
 	c = df.groupby(['COD_ATIV_CURRIC']).size()
 	diciplinas = df.groupby(['COD_ATIV_CURRIC','SIGLA']).size().reset_index(name='counts')
-	i=diciplinas.groupby(['COD_ATIV_CURRIC','SIGLA']).apply(lambda x: x['counts'] / (c[x['COD_ATIV_CURRIC']].values[0])).reset_index(name='taxas')
+	i=diciplinas.groupby(['COD_ATIV_CURRIC','SIGLA','counts']).apply(lambda x: func(x,matr)).reset_index(name='taxas gerais')
 	print_analise(i)
+
+matr = qnt_matr(df)
 analise(df)
 
 
@@ -44,5 +59,3 @@ analise(df)
 ##  c = lambda x: x+1
 ## curses = df['COD_ATIV_CURRIC'].drop_duplicates()
 ## 'MATR_ALUNO','
-#p
-#df = pd.read_excel("../base/historico.xls")
