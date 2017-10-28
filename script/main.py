e0edafb7269dc256c785667086fd756e541c7660
odair mario <badwolves123@gmail.com>
2017-10-28 13:11:50 -0200

    Revert "teste"
    This reverts commit 4fcd640e0682fcea4b2b065e28aa02aa4c9ce9e1.

--- script/main.py
+++ script/main.py
@@ -4,13 +4,15 @@ from base.dataframe_base import load_dataframes
 from build_cache import build_cache
 from datetime import timedelta
 from utils.situations import *
-from analysis.course_analysis import *
+
 def main():
     start_time = time.clock()
     start_time_exec = time.time()
+
     dataframe = load_dataframes(os.getcwd() + '/' + 'base')
+
     build_cache(dataframe)
-    Main(dataframe)
+
     cpu_time = timedelta(seconds=round(time.clock() - start_time))
     run_time = timedelta(seconds=round(time.time() - start_time_exec))
     print("--- Tempo de CPU: {} ---".format(cpu_time))
