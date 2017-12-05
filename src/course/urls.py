76267383d83da848af88c6dbf2daa36f985a3da7
Bruno Meyer <buba.meyer_@hotmail.com>
2017-12-05 08:54:21 -0200

    upload system

--- src/course/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from course import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^compare', views.compare, name='compare'),
-    url(r'^(?P<course_code>\w+)/$', views.detail, name='detail'),
-]
--- src/public/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from django.contrib.auth.views import login
-from public import views
-
-urlpatterns = [
-    url(r'^$', login, {'template_name':'public/index.html'}, name="index"),
-]
