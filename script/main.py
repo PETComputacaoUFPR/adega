42ddefb17f2e8f13e6d4de266da883e01b696390
Odair Mario <badwolves123@gmail.com>
2018-02-28 11:33:40 -0300

    merge back-to- origins, students, course, degree

--- script/main.py
+++ script/main.py
@@ -3,15 +3,9 @@ import time
 from base.dataframe_base import load_dataframes
 from build_cache import build_cache
 from datetime import timedelta
-<<<<<<< HEAD
 from analysis.degree_analysis import *
-
-
-
-=======
 from utils.situations import *
 from analysis.course_analysis import *
->>>>>>> origin/course
 def main():
     start_time = time.clock()
     start_time_exec = time.time()
@@ -19,7 +13,7 @@ def main():
     dataframe = load_dataframes(os.getcwd() + '/' + 'base')
     build_cache(dataframe)
     cpu_time = timedelta(seconds=round(time.clock() - start_time))
-    Main(dataframe)
+    analises_disciplinas(dataframe)
     run_time = timedelta(seconds=round(time.time() - start_time_exec))
     print("--- Tempo de CPU: {} ---".format(cpu_time))
     print("--- Tempo total: {} ---".format(run_time))
