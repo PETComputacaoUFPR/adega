027a8f6c64e5c7c8effb2deac71b715cea325465
Jomaro Rodrigues <jomaro.rodrigues@gmail.com>
2018-03-06 16:42:00 -0300

    analises feitas de maneira modular

--- src/script/main.py
+++ src/script/main.py
@@ -3,25 +3,38 @@ import time
 from datetime import timedelta
 from script.base.dataframe_base import load_dataframes
 from script.build_cache import build_cache
-from script.analysis.degree_analysis import *
 
+
+def analyze(submission):
+    start_time = time.clock()
+    start_time_exec = time.time()
+
+    dataframe = load_dataframes(submission.path())
+
+    build_cache(dataframe)
+
+    cpu_time = timedelta(seconds=round(time.clock() - start_time))
+    run_time = timedelta(seconds=round(time.time() - start_time_exec))
+    print("--- Tempo de CPU: {} ---".format(cpu_time))
+    print("--- Tempo total: {} ---".format(run_time))
 
 
 def main():
-	start_time = time.clock()
-	start_time_exec = time.time()
+    start_time = time.clock()
+    start_time_exec = time.time()
 
-	dataframe = load_dataframes(os.getcwd() + '/script/' + 'base')
-	#~ for i, line in enumerate(dataframe):
-		#~ print(type(dataframe["MEDIA_FINAL"][i]))
-		#~ print(dataframe["MEDIA_FINAL"][i])
-	#~ print(dataframe)
-	build_cache(dataframe)
+    dataframe = load_dataframes(os.getcwd() + '/script/' + 'base')
+    # ~ for i, line in enumerate(dataframe):
+    # ~ print(type(dataframe["MEDIA_FINAL"][i]))
+    # ~ print(dataframe["MEDIA_FINAL"][i])
+    # ~ print(dataframe)
+    build_cache(dataframe)
 
-	cpu_time = timedelta(seconds=round(time.clock() - start_time))
-	run_time = timedelta(seconds=round(time.time() - start_time_exec))
-	print("--- Tempo de CPU: {} ---".format(cpu_time))
-	print("--- Tempo total: {} ---".format(run_time))
+    cpu_time = timedelta(seconds=round(time.clock() - start_time))
+    run_time = timedelta(seconds=round(time.time() - start_time_exec))
+    print("--- Tempo de CPU: {} ---".format(cpu_time))
+    print("--- Tempo total: {} ---".format(run_time))
+
 
 if __name__ == "__main__":
-	main()
+    main()
--- src/uploads/settings.py
+++ src/uploads/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'uploads.core'
+    'uploads.core',
+    'uploads'
 ]
 
 MIDDLEWARE_CLASSES = [
