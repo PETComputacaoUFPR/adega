2d2504444ef779c412b6ced539015e7a75ea40a6
Jomaro Rodrigues <jomaro.rodrigues@gmail.com>
2018-03-18 11:13:01 -0300

    funcionando com tempo de processamento

--- src/script/main.py
+++ src/script/main.py
@@ -2,12 +2,11 @@ import os
 import time
 from script.base.dataframe_base import load_dataframes
 from script.build_cache import build_cache
-from datetime import timedelta
-from script.analysis.degree_analysis import *
-from script.utils.situations import *
 from script.analysis.course_analysis import *
 
 from datetime import timedelta
+
+
 def analyze(submission):
     start_time = time.clock()
     start_time_exec = time.time()
@@ -16,10 +15,14 @@ def analyze(submission):
 
     build_cache(dataframe)
 
+    submission.process_time = round(time.clock() - start_time)
+    submission.save()
+
     cpu_time = timedelta(seconds=round(time.clock() - start_time))
     run_time = timedelta(seconds=round(time.time() - start_time_exec))
     print("--- Tempo de CPU: {} ---".format(cpu_time))
     print("--- Tempo total: {} ---".format(run_time))
+
 
 def main():
     start_time = time.clock()
