554ec3c92d24c9ff0db5dd687771d857d2c731f8
Odair Mario <omdj17@inf.ufpr.br>
2018-06-18 21:31:36 -0300

    adiciona funçoes da dashboard e inicia classe da views degree

--- src/degree/models.py
+++ src/degree/models.py
@@ -1,5 +1,5 @@
-from django.db import models
-from django.contrib.auth.models import User
+
+from django.db import modelsfrom django.contrib.auth.models import User
 
 class Degree(models.Model):
     name = models.CharField(max_length=40)
@@ -8,7 +8,7 @@ class Degree(models.Model):
 
     manager = models.ForeignKey(User)
     def __str__(self):
-        return self.code
+        return self.name
     def clean_code(self):
         if '/' in self.code:
             raise ValidationError('Valor inválido: O código não pode conter "/"')
--- src/degree/views.py
+++ src/degree/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import view
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout as process_logout
 
 # Create your views here.
+
+@login_required
+def setDegree(request,degree_id):
+    request.session["degree"] = degree_id 
+    return redirect('public:index' ) 
+
+@login_required
+def Degreeindex(request):
+    if(!("degree" in request.session)):
+        return redirect('dashboard') 
+
+class Views(view):
+    template_name = "index.html" 
+    @login_required
+    def setDegree(self,request,degree_id):
+        request.session["degree" ] = degree_id 
+        return redirect('degree:index' ) 
+    def index(self,request):
+        degree = Degree.objects.get(code = request.session["degree"]) 
+        submission = degree.submission
+        if (("submission" in request.session)):
+            submission = request.session["submission"] 
+            submission = Submission.objects.get(id = submission) 
+        
+         degree_path = submission.path()  
+         with open(submission.path()) as File:
+
