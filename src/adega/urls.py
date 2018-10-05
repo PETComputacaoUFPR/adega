c163e024687d0a3f291af5102e7dbac3152b6c1a
eht17 <eht17@inf.ufpr.br>
2018-10-05 13:54:41 -0300

    adicionado adega/ nas urls para o servidor

--- src/adega/urls.py
+++ src/adega/urls.py
@@ -5,19 +5,19 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.dashboard, name='dashboard'),
+    url(r'^adega/$', views.dashboard, name='dashboard'),
 
-    url(r'^uploads/', include('uploads.urls', namespace='uploads')),
+    url(r'^adega/uploads/', include('uploads.urls', namespace='uploads')),
 
-    url(r'^admission/(?P<degree_id>\w*)/', include('admission.urls', namespace='admission')),
+    url(r'^adega/admission/(?P<degree_id>\w*)/', include('admission.urls', namespace='admission')),
     
-    url(r'^course/(?P<degree_id>\w*)/', include('course.urls', namespace='course')),
+    url(r'^adega/course/(?P<degree_id>\w*)/', include('course.urls', namespace='course')),
     
-    url(r'^degree/(?P<degree_id>\w*)/', include('degree.urls', namespace='degree')),
+    url(r'^adega/degree/(?P<degree_id>\w*)/', include('degree.urls', namespace='degree')),
 
-    url(r'^public/', include('public.urls', namespace='public')),
+    url(r'^adega/public/', include('public.urls', namespace='public')),
 
-    url(r'^logout/$', views.logout, name='logout'),
+    url(r'^adega/logout/$', views.logout, name='logout'),
 
-    url(r'^admin/', admin.site.urls),
+    url(r'^adega/admin/', admin.site.urls),
 ]
