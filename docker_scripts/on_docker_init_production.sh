d77f165129dd1a9bb0545aaf6a8c837fab8e22e2
eht17 <eht17@inf.ufpr.br>
2018-10-05 09:17:42 -0300

    formatação

--- docker_scripts/on_docker_init_production.sh
+++ docker_scripts/on_docker_init_production.sh
@@ -4,8 +4,8 @@ bash ./docker_scripts/wait_for_postgres.sh
 cd src
 python manage.py makemigrations degree admission educator uploads course
 python manage.py migrate
-#python manage.py runserver 0.0.0.0:8000
 
 python manage.py collectstatic
 chmod 775 -R adega/static
+#python manage.py runserver 0.0.0.0:8000
 gunicorn adega.wsgi:application -w 2 -b :8000
