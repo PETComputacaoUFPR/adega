fab5c209fa3e361145ecbb1ffb8995bea5ecca35
eht17 <eht17@inf.ufpr.br>
2018-10-08 17:35:19 -0300

    adicionado redirecionamento da url padrão para /adega/

--- src/adega/urls.py
+++ src/adega/urls.py
@@ -4,15 +4,19 @@ from django.contrib import admin
 
 from . import views
 
+from django.views.generic import RedirectView
+
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url='/adega/')),
+
     url(r'^adega/$', views.dashboard, name='dashboard'),
 
     url(r'^adega/uploads/', include('uploads.urls', namespace='uploads')),
 
     url(r'^adega/admission/(?P<degree_id>\w*)/', include('admission.urls', namespace='admission')),
-    
+
     url(r'^adega/course/(?P<degree_id>\w*)/', include('course.urls', namespace='course')),
-    
+
     url(r'^adega/degree/(?P<degree_id>\w*)/', include('degree.urls', namespace='degree')),
 
     url(r'^adega/public/', include('public.urls', namespace='public')),
