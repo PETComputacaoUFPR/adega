3718be63f0c87d455b7cf6a2a1adf9db75508a61
Odair M <odairmario45@gmail.com>
2019-05-31 00:08:01 -0300

    metodo que retorna todas as disciplinas

--- src/grid/models.py
+++ src/grid/models.py
@@ -1,40 +1,51 @@
 from django.db import models
 from degree.models import Degree
 
+
 class Grid(models.Model):
     version = models.IntegerField()
-    degree = models.ForeignKey(Degree,
+    degree = models.ForeignKey(
+            Degree,
             on_delete=models.CASCADE,
-            related_name = "grids",
-            related_query_name = "grids",
+            related_name="grids",
+            related_query_name="grids",
             )
 
     def __str__(self):
         return "Curso: {} Versão: {}".format(self.degree, self.version)
+
+    def get_courses(self):
+        courses = []
+        for period in self.periods.all():
+            courses += period.courses.all()
+        return courses
+
+
 class GridPeriod(models.Model):
     number = models.IntegerField()
-    grid = models.ForeignKey(Grid,
+    grid = models.ForeignKey(
+            Grid,
             on_delete=models.CASCADE,
-            related_name = "period",
-            related_query_name = "period"
+            related_name="periods",
+            related_query_name="periods"
             )
-    
+
     def __str__(self):
-        return "Periodo: {}\nDisciplinas:\n {}".format(self.number,
+        return "Periodo: {}\nDisciplinas:\n {}".format(
+                self.number,
                 self.courses.all())
+
 
 class GridCourse(models.Model):
     name = models.CharField(max_length=200)
+    _type = models.CharField(max_length=10)
     code = models.CharField(max_length=10)
-    period = models.ForeignKey(GridPeriod,
+    period = models.ForeignKey(
+            GridPeriod,
             on_delete=models.CASCADE,
-            related_name = "courses",
-            related_query_name = "courses",
+            related_name="courses",
+            related_query_name="courses",
             )
 
     def __str__(self):
         return "{} {}".format(self.name, self.code)
-
-
-    #period_type = models.IntegerField()
-    
