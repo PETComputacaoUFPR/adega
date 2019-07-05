2c379415b7d1a1bed91051d9f51102b70f8491bf
pedro <pedropasq@ymail.com>
2019-07-05 11:44:44 -0300

    adega#198: Fix bug do dashboard em que relatórios deletados ainda eram exibidos, e otimizado o texto do dashboard.

--- src/adega/views.py
+++ src/adega/views.py
@@ -14,11 +14,12 @@ def dashboard(request):
         last_submission = Submission.objects.filter(
             degree=degree
         ).last()
-        degrees_last_submissions.append({
-            "name": degree.name,
-            "code": degree.code,
-            "last_submission": last_submission
-        })
+        if not (last_submission is None):
+            degrees_last_submissions.append({
+                "name": degree.name,
+                "code": degree.code,
+                "last_submission": last_submission
+            })
     return render(request, 'adega/dashboard.html', {"title": "Dashboard",
                                                     "degrees_last_submissions":degrees_last_submissions,
                                                     "hide_navbar": True
