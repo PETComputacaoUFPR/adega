cf5f0df149617f9b6c18083f106d91115de7a3a4
lemf19 <lemf19@inf.ufpr.br>
2019-09-06 11:31:04 -0300

    adega#212 Trabalhando no backend do fack

--- src/faq/models.py
+++ src/faq/models.py
@@ -1,12 +1,16 @@
 from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     question = models.CharField(max_length=400)
     answer = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
+        self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
--- src/faq/views.py
+++ src/faq/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Question 
+from django.utils import timezone
 
 # Create your views here.
 
 @login_required
 def question_list(request):
-    
-    return render(request, 'faq/question_list.html', {"title": "FAQ",
-                                                     "hide_navbar": True
+    questions = Question.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'faq/question_list.html', {"title" : "FAQ",
+                                                     "hide_navbar" : True,
+                                                     'questions' : questions
                                                     })
