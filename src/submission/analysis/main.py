6f91496214db3784544b3ec343cfff4a391f6561
bhmeyer <bhmeyer@inf.ufpr.br>
2020-08-12 20:50:43 -0300

    Revert "Merge branch '218-criar-modulo-da-interface-que-converta-adega-para-weka' into 'development'"
    This reverts merge request !127

--- src/submission/analysis/main.py
+++ src/submission/analysis/main.py
@@ -6,25 +6,18 @@ import traceback
 
 import pandas as pd
 
-from submission.analysis.json_submission_to_csv.main import main as parse_to_csv
-
-def analyze(submission, debug=True, create_csv_data=True):
+def analyze(submission, debug=True):
     start_time = time.clock()
     start_time_exec = time.time()
     submission.set_executing()
     try:
         path = submission.path()
-        
-        # TODO: Remove this line when support is deprecated
-        # dataframe = load_dataframes(path) # OLD VERSION.
+        # dataframe = load_dataframes(path) # OLD VERSION. TODO: Remove this line when support is deprecated
         # dataframe = pd.read_csv(submission.csv_data_file.path)
         dataframe = read_processed_csv(submission.csv_data_file.path)
         build_cache(dataframe, path, submission.degree.code,
                     submission.relative_year, submission.relative_semester)
-        
-        if create_csv_data:
-            parse_to_csv(path, submission.zip_path())
-        
+
         submission.set_done(round(time.clock() - start_time))
 
         cpu_time = timedelta(seconds=round(time.clock() - start_time))
