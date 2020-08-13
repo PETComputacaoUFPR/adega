c9aaa50b6bb1382b7128555d01d2c0048dfb36a6
bhmeyer <bhmeyer@inf.ufpr.br>
2020-08-13 02:05:34 -0300

    #218:Criado um arquivo que atualmente tem duas funções, uma pra listar os jsons de um path e uma pra criar cabeçalhos das planilhas csvs (ainda não completo)

--- src/adega/views.py
+++ src/adega/views.py
@@ -18,12 +18,17 @@ def dashboard(request):
             degrees_last_submissions.append({
                 "name": degree.name,
                 "code": degree.code,
-                "last_submission": last_submission
+                "last_submission": last_submission,
+                "download_allowed": last_submission.download_allowed(request.user)
             })
-    return render(request, 'adega/dashboard.html', {"title": "Dashboard",
-                                                    "degrees_last_submissions":degrees_last_submissions,
-                                                    "hide_navbar": True
-                                                    })
+    return render(request,
+                  'adega/dashboard.html',
+                  {
+                    "title": "Dashboard",
+                    "degrees_last_submissions":degrees_last_submissions,
+                    "hide_navbar": True
+                  }
+    )
 
 
 @login_required
